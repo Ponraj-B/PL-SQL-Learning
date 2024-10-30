535b61e399007e848ce46339f72471b8c102352c
Ponraj_B <ponraj322002@gmail.com>
2024-10-30 15:48:59 +0530

    Update Calculate the Number of Days Between Two Dates.sql

--- Anonymous blocks/Date programs/Calculate the Number of Days Between Two Dates.sql
+++ Anonymous blocks/Date programs/Calculate the Number of Days Between Two Dates.sql
@@ -1,1 +1,9 @@
-hi
+set serveroutput on;
+DECLARE
+  date1 date := &in1;
+  date2 date := &in2;
+BEGIN 
+  dbms_output.put_line('Days between ' || date1 || ' and ' || 'date2 is ' || 
+  (date1-date2)); 
+END;
+/
